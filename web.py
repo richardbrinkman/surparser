2a712416f759f35dc32fbf610c6337cfb6405dc8
Richard Brinkman <r.brinkman@saxion.nl>
2020-03-08 10:46:47 +0100

    Add: missing web interface

---
+++ web.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pypandoc
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    _, output_formats = pypandoc.get_pandoc_formats()
+    return render_template("index.html", output_formats=output_formats)
+
+
+@app.route("/convert", methods=["POST"])
+def convert():
+    result = ""
+    for key in request.form:
+        result += "{} == {}\n".format(key, request.form[key])
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
