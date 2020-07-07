53c4298d342d8100935e538bf920a8faf421d991
Richard Brinkman <r.brinkman@saxion.nl>
2020-07-07 17:51:19 +0200

    Add: toetsinzage.py

---
+++ toetsinzage.py
@@ -0,0 +1,29 @@
+import argparse
+import pandas
+
+
+argument_parser = argparse.ArgumentParser(description="""
+    Converter of ItemDeliveredRawReport.csv to toetsinzage.xlsx.
+
+    Given an ItemsDeliveredRawReport.csv file produced by Surpass, this
+    tool generates an Excel file that can be used to schedule a
+    toetsinzage.
+""")
+argument_parser.add_argument("--input",
+    default="ItemsDeliveredRawReport.csv",
+    help="Name of the input CSV file (defaults to ItemsDeliveredRawReport.csv)",
+    metavar="input_file_name.csv"
+)
+argument_parser.add_argument("--output",
+    default="toetsinzage.xlsx",
+    help="Name of the generated Excel file (defaults to toetsinzage.xlsx)",
+    metavar="output_file_name.xlsx"
+)
+parser = argument_parser.parse_args()
+
+input_df = pandas.read_csv(parser.input)
+df = input_df[input_df["Grade"] != "Ongeldig"]
+output_df = df[["FirstName", "LastName", "Keycode"]]
+pandas.options.mode.chained_assignment = None
+output_df["Email"] = df["Reference"].astype(str) + "@student.saxion.nl"
+output_df.to_excel(parser.output, index=False)
